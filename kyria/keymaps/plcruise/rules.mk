fa6432dee0dffeab93e3a4196b5d1c4fd0aaa9b5
pkruse <pa.kruse@ostfalia.de>
2023-05-11 22:29:37 +0200

    :sparkles: add keymap for rev 1

---
+++ kyria/keymaps/plcruise/rules.mk
@@ -0,0 +1,7 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+AUTO_SHIFT_ENABLE = no    # Enable the autoshift option
+COMMAND_ENABLE   = no # Commands for debug and configuration
+UNICODE_ENABLE   = no# Unicode
+MOUSEKEY_ENABLE  = no # Mouse keys(+4700)
---
+++ kyria/keymaps/plcruise_ger/rules.mk
@@ -0,0 +1,8 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+COMMAND_ENABLE = yes	   # Enable the use of command; In the wirst throw this is for experimental purposes as it is not known what specifically this does.
+AUTO_SHIFT_ENABLE = no   # Enable the autoshift option
+COMMAND_ENABLE   = no # Commands for debug and configuration
+UNICODE_ENABLE   = no# Unicode
+MOUSEKEY_ENABLE  = no # Mouse keys(+4700)
